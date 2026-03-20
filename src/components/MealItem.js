172522b18e6deb758cc7bcdafc1d5191d6b3e188
MarkusErkmann <markus.erkmann@voco.ee>
2026-03-20 11:07:23 +0200

    Exercise 5

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -8,7 +8,12 @@ const MealItem = (props) => {
         />
         <div>
           <h3>{props.meal.name}</h3>
-          <p className="meal-item-price">{props.meal.price}</p>
+          <p className="meal-item-price">
+            {Intl.NumberFormat("de-DE", {
+              style: "currency",
+              currency: "EUR",
+            }).format(Number(props.meal.price))}
+          </p>
           <p className="meal-item-description">{props.meal.description}</p>
         </div>
         <p className="meal-item-actions">
