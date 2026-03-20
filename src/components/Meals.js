df74f247aeeac7fd3aba99bc2213ad717562e9ba
MarkusErkmann <markus.erkmann@voco.ee>
2026-03-20 10:50:07 +0200

    Exercise 3

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,12 +1,26 @@
-const Meals = () => {
-    return (
-        <ul id="meals">
-            <h2>create list of meals, using fetch data from backend</h2>
-            { 
-               
-            }
-        </ul>
-    )
-}
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default Meals
+const Meals = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/meals")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <ul id="meals">
+      <h2>Create list of meals</h2>
+      {}
+    </ul>
+  );
+};
+
+export default Meals;
