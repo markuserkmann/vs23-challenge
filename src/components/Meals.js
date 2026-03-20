b33cf0345ea6924f0b2d85b495c8783cdeda99c3
MarkusErkmann <markus.erkmann@voco.ee>
2026-03-20 10:38:57 +0200

    Exercise 1 completed

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,8 +1,9 @@
 const Meals = () => {
     return (
         <ul id="meals">
+            <h2>create list of meals, using fetch data from backend</h2>
             { 
-                // list of meals
+               
             }
         </ul>
     )
